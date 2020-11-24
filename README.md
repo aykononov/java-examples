54ab4e9478d147400bd9e95c7fbb287e8b6a0454
aykononov <aykononov@yandex.ru>
2020-11-24 21:25:36 +0300

    new

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# JavaExamples
+## Примеры Java
+
+[ListFiles01/ListFilesDemo.java - Вывести списокв файлов](https://github.com/aykononov/JavaExamples/tree/main/src/main/java/ListFiles01/ListFilesDemo.java "Посмотреть пример Java")
+
---
+++ src/main/java/ListFiles01/ListFilesDemo.java
@@ -0,0 +1,38 @@
+package ListFiles01;
+
+import java.io.File;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+// Пример выводит все файлы в указанной директории + фильтр.
+
+class ListFiles {
+
+    public Set<String> listFilesUsingJavaIO(String dir) {
+        return Stream.of(new File(dir).listFiles())
+                //.filter(file -> !file.isDirectory())
+                .filter(file -> !file.isDirectory() &&  // фильтр: файл не является директорий
+                        file.getName().startsWith("ListFilesDemo")) // фильтр: имя файла начинается с "..."
+                .map(File::getName)
+                .collect(Collectors.toSet());
+    }
+}
+
+public class ListFilesDemo {
+
+    public static void main(String[] args) {
+        String dir ="src/main/java/ListFiles01/";
+        ListFiles listFiles = new ListFiles();
+        System.out.println("Перечислить файлы в директории: " + dir + "\n..");
+        for (String checkFile : listFiles.listFilesUsingJavaIO(dir)) {
+            System.out.println(checkFile);
+        }
+    }
+}
+
+/* -------------------------------------------
+Перечислить файлы в директории: src/main/java/ListFiles01/
+..
+ListFilesDemo.java
+ */
